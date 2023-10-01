e96b33cf885ab9fe5c1fd165f98bed31a59dc19b
Matthew Rodgers <48635776+AxisMaen@users.noreply.github.com>
2023-10-01 18:10:42 -0400

    FM-6 - food search system test

--- src/tests/foodItemApiClient_test.py
+++ src/tests/foodItemApiClient_test.py
@@ -1,4 +1,5 @@
 from clients.foodItemApiClient import FoodItemApiClient
+from models.searchTableModel import SearchTableModel
 
 
 # Test ID: UT-1
@@ -37,3 +38,31 @@ def test_good_response(requests_mock):
 
     # ensure the mock response is returned
     assert response == mockResponse
+
+
+# Test ID: ST-1
+def test_access_error_system(requests_mock):
+    client = FoodItemApiClient()
+    model = SearchTableModel()
+
+    mockResponse = {"mock": "mock response"}
+
+    # data that is expected to be in the model
+    expectedDisplay = {
+        "error": "Unauthorized: Please verify your API key is correct or try again later"
+    }
+
+    # create the mock response with an authorization error
+    requests_mock.get(
+        client.baseUrl + client.endpoints["ingredients"],
+        status_code=401,
+        json=mockResponse,
+    )
+
+    model.search("test")
+
+    # error should be present in foodData
+    actualDisplay = model.foodData[0]
+
+    # ensure the error appears in the model
+    assert actualDisplay == expectedDisplay
