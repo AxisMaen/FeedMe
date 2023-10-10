29ba4aed3d0861b7795a2a1aced8278d6b878ece
Matthew Rodgers <48635776+AxisMaen@users.noreply.github.com>
2023-10-09 21:02:39 -0400

    FM-11 - remove from shopping list unit tests

--- src/tests/shoppingModel_test.py
+++ src/tests/shoppingModel_test.py
@@ -85,3 +85,82 @@ def test_add_search_items_to_shopping_list(requests_mock):
 
     # ensure that food items were added to the model
     assert shoppingModel.foodData == mockModelData
+
+
+# Test ID: UT-7
+def test_remove_from_shopping_list(requests_mock):
+    model = ShoppingTableModel()
+
+    mockFoodItems = [
+        {"id": 1, "name": "banana", "pixmap": None},
+        {"id": 2, "name": "tomato", "pixmap": None},
+    ]
+
+    model.addFoodItems(mockFoodItems)
+
+    # remove the first mock food item
+    model.removeFoodItems([0])
+
+    assert model.foodData == [mockFoodItems[1]]
+
+
+# Test ID: UT-8
+def test_remove_nothing_from_shopping_list(requests_mock):
+    model = ShoppingTableModel()
+
+    mockFoodItems = [
+        {"id": 1, "name": "banana", "pixmap": None},
+        {"id": 2, "name": "tomato", "pixmap": None},
+    ]
+
+    model.addFoodItems(mockFoodItems)
+
+    # pass no indexes to remove function
+    model.removeFoodItems([])
+
+    assert model.foodData == mockFoodItems
+
+
+# Test ID: ST-4
+def test_remove_search_items_from_shopping_list(requests_mock):
+    # QApplication needed for some PyQt elements to not hang tests
+    qApp = QApplication(sys.argv)
+
+    searchModel = SearchTableModel()
+    shoppingModel = ShoppingTableModel()
+
+    controller = FoodItemController()
+
+    mockResponse = {
+        "results": [
+            {"id": 1, "name": "banana", "image": "test.jpg"},
+            {"id": 2, "name": "tomato", "image": "test.jpg"},
+        ]
+    }
+
+    mockModelData = [{"id": 2, "name": "Tomato"}]
+
+    # create the mock response with a success code
+    requests_mock.get(
+        controller.client.baseUrl + controller.client.endpoints["ingredients"],
+        status_code=200,
+        json=mockResponse,
+    )
+
+    # get mock food data
+    searchModel.search("test")
+
+    foodData = searchModel.foodData
+
+    # add mock food data to shopping model
+    shoppingModel.addFoodItems(foodData)
+
+    # remove the first mock food item
+    shoppingModel.removeFoodItems([0])
+
+    # remove pixmaps from the comparison for simplicity
+    shoppingModelData = shoppingModel.foodData
+    for item in shoppingModelData:
+        item.pop("pixmap")
+
+    assert shoppingModelData == mockModelData
